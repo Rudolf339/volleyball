bbfdcfbdd5dd2a4471defecceb744c09df10cc10
Rudolf339 <juttner.domokos@gmail.com>
2020-02-20 23:59:37 +0100

    Ready to rock \o/

--- data.py
+++ data.py
@@ -1,12 +1,11 @@
-import csv
-import vb
+import csv, vb, os
 
 match_csv = []
 with open('./meccsek.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=';', quotechar='|')
     for row in reader:
         match_csv.append(row)
-        
+
 group_csv = []
 with open('./csoportok.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=';', quotechar='|')
@@ -25,7 +24,6 @@ with open('./csoportok.csv') as csvfile:
             except IndexError:
                 raw_csv[row].append('')
 
-    print()
     # flip cloumns and rows
     for c in range(ml):
         group_csv.append([])
@@ -44,21 +42,28 @@ data = {
 abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']  # theoretical support of 8 pools
 for g in range(len(group_csv)):
     data['groups'][abc[g]] = {1:'', 2:'', 3:''}
-    for t in match_csv[g]:
+    for t in group_csv[g]:
         data['teams'][t] = {'group':abc[g], 'points':0, 'wins':0}
 
 matches = []
+data['GM'] = ''
+gm = 0
 for m in match_csv:
-    t = m[0]
+    st = m[0]
     r = m[1]
     l = m[2]
-    winner = ''
-    matches.append({'t':t, 'r':r, 'l':l, 'w':''})
+    t = m[3]
+    if (r.startswith('#') or l.startswith('#')) and data['GM'] == '':
+        data['GM'] = gm
+    else:
+        gm += 1
+    matches.append({'st':st, 't':t, 'r':r, 'l':l, 'w':'', 'loser':''})
 
 data['matches'] = matches
 
 def order(data):
     for g in data['groups']:
+        go = True
         t = {}
         # add group members to t
         for tn in data['teams']:
@@ -67,8 +72,90 @@ def order(data):
         s = sorted(t, key=lambda x: (t[x]['wins'],
                                      t[x]['points']),
                    reverse=True)
-        data['groups'][g][1] = s[0]
-        data['groups'][g][2] = s[1]
-        data['groups'][g][3] = s[2]
-        
+        for n in data['groups'][g]:
+            if data['groups'][g][n] != '':
+                go = False
+        if go:
+            data['groups'][g][1] = s[0]
+            data['groups'][g][2] = s[1]
+            data['groups'][g][3] = s[2]
+
+    for m in range(data['current_round'], len(data['matches'])):
+        if data['matches'][m]['r'].startswith('#'):
+            if data['matches'][m]['r'][1] == 'G':
+                data['matches'][m]['r'] = data['groups'][data['matches'][m]['r'][2]][int(data['matches'][m]['r'][3])]
+            
+            elif data['matches'][m]['r'][3] == 'W' and data['matches'][int(data['matches'][m]['r'][1]) * 10 + int(data['matches'][m]['r'][2]) - 1]['w'] != '':
+                data['matches'][m]['r'] = data['matches'][int(data['matches'][m]['r'][1]) * 10 + int(data['matches'][m]['r'][2]) - 1]['w']
+            elif data['matches'][m]['r'][3] == 'L' and data['matches'][int(data['matches'][m]['r'][1]) * 10 + int(data['matches'][m]['r'][2]) - 1]['loser'] != '':
+                data['matches'][m]['r'] = data['matches'][int(data['matches'][m]['r'][1]) * 10 + int(data['matches'][m]['r'][2]) - 1]['loser']
+
+        if data['matches'][m]['l'].startswith('#'):
+            if data['matches'][m]['l'][1] == 'G':
+                data['matches'][m]['l'] = data['groups'][data['matches'][m]['l'][2]][int(data['matches'][m]['l'][3])]
+
+            elif data['matches'][m]['l'][3] == 'W' and data['matches'][int(data['matches'][m]['l'][1]) * 10 + int(data['matches'][m]['l'][2]) - 1]['w'] != '':
+                data['matches'][m]['l'] = data['matches'][int(data['matches'][m]['l'][1]) * 10 + int(data['matches'][m]['l'][2]) - 1]['w']
+            elif data['matches'][m]['l'][3] == 'L' and data['matches'][int(data['matches'][m]['l'][1]) * 10 + int(data['matches'][m]['l'][2]) - 1]['loser'] != '':
+                data['matches'][m]['l'] = data['matches'][int(data['matches'][m]['l'][1]) * 10 + int(data['matches'][m]['l'][2]) - 1]['loser']
+
     return data
+
+def export(db):
+    _ = ' | '
+    md = '####Éjszakai Röpi 2020\n'
+    md += '| csapatnév            | csoport | nyert | pont |\n'
+    md += '|----------------------------------|---|---|----|\n'
+    for t in db['teams'].keys():
+        if t != '#K_D' and t != 'foo':
+            md += '| ' + t  + ' ' * (32 - len(t)) + _ + db['teams'][t]['group'] + _ + str(db['teams'][t]['wins']) + _ + str(db['teams'][t]['points']) + ' |\n'
+
+    md += '\n'
+    md += '## Meccsek\n'
+    md += 'Félkövér a győztes\n'
+    md += '| kezdés | csapatnév                      | csapatnév                      | kategória |\n'
+    md += '|--------|--------------------------------|--------------------------------|---------------|\n'
+    for m in db['matches']:
+        if not m['l'].startswith('#'):
+            if m['l'] != 'K_D':
+                if m['l'] == m['w']:
+                    l = '__' + m['l'] + '__'
+                else:
+                    l = m['l']
+            else:
+                l = 'Közönségszavazatos'
+        else:
+            if m['l'][1] == 'G':
+                l = m['l'][2] + ' csoport ' +  m['l'][3]
+            else:
+                n = int(m['l'][1]) * 10 + int(m['l'][2])
+                l = db['matches'][n]['st'] + '-es meccs '
+                if m['l'][3] == 'W':
+                    l += 'győztese'
+                else:
+                    l += 'vesztese'
+        if not m['r'].startswith('#'):
+            if m['r'] != 'K_D':
+                if m['r'] == m['w']:
+                    r = '__' + m['r'] + '__'
+                else:
+                    r = m['r']
+            else:
+                r = 'Közönségszavazatos'
+        else:
+            if m['r'][1] == 'G':
+                r = m['r'][2] + ' csoport ' + m['r'][3]
+            else:
+                r = db['matches'][int(m['r'][1]) * 10 + int(m['r'][2])]['st'] + '-es meccs '
+                if m['r'][3] == 'W':
+                    r += 'győztese'
+                else:
+                    r += 'vesztese'
+        l += ' ' * (32 - len(l))
+        r += ' ' * (32 -len(r))
+        md += '| ' + m['st'] + _ + l + _ + r + _ + m['t'] + ' |\n'
+
+    with open('table.md', 'w') as f:
+        f.write(md)
+    os.system('markdown2 table.md > table.html')
+    
--- scoreboard.py
+++ scoreboard.py
@@ -3,6 +3,11 @@ import time
 import curses
 import data
 import json
+import random
+
+DEBUG = True
+QUICK = True
+carryOn = True
 
 try:
     with open('progress_dump.json', 'r') as json_file:
@@ -16,12 +21,28 @@ try:
                 break
             elif ans.lower().startswith('n'):
                 db = data.data
+                db['current_round'] = 0
                 break
             else:
                 print('Invalid answer')
 
 except IOError:
     db = data.data
+    db['current_round'] = 0
+
+if DEBUG:
+    print('teams')
+    for a in db['teams']:
+        print('\t', a, db['teams'][a])
+    print('groups')
+    for a in db['groups']:
+        print('\t', a, db['groups'][a])
+    print('matches')
+    for a in db['matches']:
+        print('\t', a)
+    print('GM:', db['GM'])
+    if input() == 'q':
+        carryOn = False
 
 scrn = curses.initscr()
 
@@ -40,23 +61,22 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-scoreFont = pygame.font.SysFont('./font/Cousine-Regular.ttf', 500)
-teamFont = pygame.font.SysFont('./font/Cousine-Regular.ttf', 120)
-upcomingFont = pygame.font.SysFont('./font/Cousine-Regular.ttf', 50)
-timeFont = pygame.font.SysFont('./font/Cousine-Regular.ttf', 400)
+scoreFont = pygame.font.SysFont('./font/Cousine-Regular.ttf', 300)
+teamFont = pygame.font.SysFont('./font/Cousine-Regular.ttf', 60)
+upcomingFont = pygame.font.SysFont('./font/Cousine-Regular.ttf', 30)
+timeFont = pygame.font.SysFont('./font/Cousine-Regular.ttf', 250)
 
-X = 1920
-Y = 1080
+X = 1024
+Y = 768
 
 games = []
 for g in range(db['current_round'], len(db['matches'])):
-    games.append((db['matches'][g]['l'], db['matches'][g]['r']))
+    games.append((db['matches'][g]['l'], db['matches'][g]['r'],
+                 db['matches'][g]['t'], db['matches'][g]['st']))
 
 screen = pygame.display.set_mode((X, Y))
 pygame.display.set_caption('ponttábla')
 
-
-carryOn = True
 timer = False
 t = 0  # seconds
 clock = pygame.time.Clock()
@@ -85,47 +105,39 @@ def next_round():
     games.remove(games[0])
 
 nxt = False
-
+cmd = ''
+cmd_mode = False
+out = ''
 c = time.time()
 # -------- Main Program Loop -----------
 while carryOn:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             carryOn = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT and not event.mod & pygame.KMOD_SHIFT:
-                l += 1
-            elif event.key == pygame.K_RIGHT:
-                l += -1
-            elif event.key == pygame.K_LEFT and not event.mod & pygame.KMOD_SHIFT:
-                r += 1
-            elif event.key == pygame.K_LEFT:
-                r += -1
-            elif event.key == pygame.K_SPACE:
-                timer = not timer
-            elif event.key == pygame.K_r and event.mod & pygame.KMOD_SHIFT:
-                t = 0
-            elif event.key == pygame.K_UP and not event.mod & pygame.KMOD_SHIFT:
-                t += 1
-            elif event.key == pygame.K_UP:
-                t += 60
-            elif event.key == pygame.K_DOWN and not event.mod & pygame.KMOD_SHIFT:
-                t += -1
-            elif event.key == pygame.K_DOWN:
-                t += -60
-            elif event.key == pygame.K_RETURN and event.mod & pygame.KMOD_CTRL:
-                next_round()
-
+            
     try:
         k = scrn.getkey()
     except:
         k = ''
 
-    if k != '':
+    if cmd_mode:
+        if k == 'KEY_DC':
+            cmd_mode = False
+        elif k == 'KEY_BACKSPACE':
+            cmd = cmd[:-1]
+            scrn.clear()
+        else:
+            cmd += k
+    
+    elif k != '':
+        if k == 's':
+            QUICK = False
         if k == 'Q':
             break
         elif k == ' ':
             timer = not timer
+        elif k == 'r':
+            scrn.clear()
         elif k == 'R':
             t = 0
         elif k == 'N':
@@ -146,7 +158,8 @@ while carryOn:
             t += -1
         elif k == 'KEY_SR':
             t += -60
-        
+        elif k == 'KEY_HOME':
+            cmd_mode = True
     if timer:
         ct = time.time()
         t += (ct - c)
@@ -158,15 +171,52 @@ while carryOn:
     if t >= 600:
         timer = False
 
+    # ----- command execution -----
+    if not cmd_mode:
+        if cmd == ('ls teams'):
+            out = ''
+            for te in db['teams']:
+                out += te + ' ' + str(db['teams'][te]) + '\n'
+        elif cmd == ('ls groups'):
+            out = ''
+            for gr in db['groups']:
+                out += gr + ' ' + str(db['groups'][gr]) + '\n'
+        elif cmd.startswith('K_D '):
+            if cmd[4:] in db['teams'].keys():
+                for m in range(db['current_round'], len(db['matches'])):
+                    if db['matches'][m]['r'] == 'K_D':
+                        db['matches'][m]['r'] = cmd[4:]
+                        out = 'Done.'
+                    elif db['matches'][m]['l'] == 'K_D':
+                        db['matches'][m]['l'] = cmd[4:]
+                        out = 'Done.'
+                
+                with open('progress_dump.json', 'w') as dumpfile:
+                    json.dump(db, dumpfile)
+            else:
+                out = 'FAILED: no team of the given name'
+        else:
+            out = 'unkown command'
+    
     # ----- pygame write -----
+    
     cl = timeFormat(t)
-    team_l = teamFont.render(games[0][0], 1, WHITE)
-    team_r = teamFont.render(games[0][1], 1, WHITE)
-    next_l = upcomingFont.render(games[1][0], 1, WHITE)
-    next_r = upcomingFont.render(games[1][1], 1, WHITE)
+    try:
+        team_l = teamFont.render(games[0][0], 1, WHITE)
+        team_r = teamFont.render(games[0][1], 1, WHITE)
+    except IndexError:
+        break
+    try:
+        next_l = upcomingFont.render(games[1][0], 1, WHITE)
+        next_r = upcomingFont.render(games[1][1], 1, WHITE)
+    except IndexError:
+        next_l = upcomingFont.render('', 1, WHITE)
+        next_r = upcomingFont.render('', 1, WHITE)
+        
     score_l = scoreFont.render(format(l), 1, WHITE)
     score_r = scoreFont.render(format(r), 1, WHITE)
     dot = scoreFont.render(':', 1, WHITE)
+    mtype = teamFont.render(games[0][2], 1, WHITE)
 
     # timer
     if t < 590 or int(t) % 2 == 1:
@@ -180,53 +230,108 @@ while carryOn:
     
     screen.fill(BLACK)
 
-    screen.blit(team_l, (X / 2 - team_l.get_width() - 100, 70))
-    screen.blit(team_r, (X / 2 + 100, 70))
+    screen.blit(team_l, (X / 2 - team_l.get_width() - 60, 40))
+    screen.blit(team_r, (X / 2 + 60, 40))
     
-    screen.blit(score_l, (X / 2 - score_l.get_width() - 150, 200))
-    screen.blit(score_r, (X / 2 + 150, 200))
-    screen.blit(dot, (X / 2 - dot.get_width() / 2, 180))
+    screen.blit(score_l, (X / 2 - score_l.get_width() - 80, 150))
+    screen.blit(score_r, (X / 2 + 80, 150))
+    screen.blit(dot, (X / 2 - dot.get_width() / 2, 150))
     
-    screen.blit(minutes, (X / 2 - minutes.get_width() - 40, 500))
-    screen.blit(seconds, (X / 2 + 50, 500))
-    screen.blit(dot2, (X / 2 - dot2.get_width() / 2, 500))
+    screen.blit(minutes, (X / 2 - minutes.get_width() - 40, 350))
+    screen.blit(seconds, (X / 2 + 50, 350))
+    screen.blit(dot2, (X / 2 - dot2.get_width() / 2, 350))
 
+    screen.blit(mtype, (X/2 - mtype.get_width() / 2, 530))
+    
     # upcoming matches
     uy = 500
     for i in range(len(games)):
         if i > 3:
             break
         elif i != 0:
-            left = upcomingFont.render(games[i][0], 1, WHITE)
-            right = upcomingFont.render(games[i][1], 1, WHITE)
-            screen.blit(left, (X / 2 - left.get_width() - 500, uy + i * 70))
-            screen.blit(right, (X / 2 + 500, uy + i * 70))
+            if games[i][0].startswith('#'):
+                left = upcomingFont.render('?????', 1, WHITE)
+            else:
+                left = upcomingFont.render(games[i][0], 1, WHITE)
+            if games[i][1].startswith('#'):
+                right = upcomingFont.render('?????', 1, WHITE)
+            else:
+                right = upcomingFont.render(games[i][1], 1, WHITE)
+            screen.blit(left, (X / 2 - left.get_width() - 200, uy + i * 70))
+            screen.blit(right, (X / 2 + 200, uy + i * 70))
 
-    if (t >= 600 and l != r) and nxt:  # There's a winner
-        if l > r:
-            db['teams'][games[0][0]]['wins'] += 1
+    if db['current_round'] > 60:
+        QUICK = False
+    if db['current_round'] < db['GM'] or QUICK:
+        if random.random() > 0.5:
+            l = 1
         else:
+            r = 1
+        nxt = True
+        t = 601
+        
+    if (t >= 600 and l != r) and nxt:  # There's a winner
+        if l > r and games[0][2] != 'tanári':
+            db['teams'][games[0][0]]['wins'] += 1
+            db['matches'][db['current_round']]['w'] = games[0][0]
+            db['matches'][db['current_round']]['loser'] = games[0][1]
+            db['teams'][games[0][0]]['points'] += l
+            db['teams'][games[0][1]]['points'] += r
+        elif l < r and games[0][2] != 'tanári':
             db['teams'][games[0][1]]['wins'] += 1
-        db['teams'][games[0][0]]['points'] += l
-        db['teams'][games[0][1]]['wins'] += r
+            db['matches'][db['current_round']]['w'] = games[0][1]
+            db['matches'][db['current_round']]['loser'] = games[0][0]
+            db['teams'][games[0][0]]['points'] += l
+            db['teams'][games[0][1]]['points'] += r
         t = 0
         l = 0
         r = 0
         timer = False
-        nxt = False
         db['current_round'] += 1
         next_round()
+        scrn.clear()
+        data.export(db)
         with open('progress_dump.json', 'w') as dumpfile:
             json.dump(db, dumpfile)
-        
+            
+    if db['current_round'] >= db['GM']:
+        db = data.order(db)
+        games = []
+        for g in range(db['current_round'], len(db['matches'])):
+            games.append((db['matches'][g]['l'], db['matches'][g]['r'],
+                          db['matches'][g]['t'], db['matches'][g]['st']))
+
     # ----- curses write -----
-    score_txt = str(r) + ' : ' + str(l)
+    score_txt = str(r) + ' : ' + str(l) + '  '
     time_txt = str(cl[0]) + ':' + str(cl[1])
     scrn.addstr(4, 4, score_txt)
     scrn.addstr(5, 4, time_txt)
-    scrn.addstr(6, 4, k)
-    scrn.addstr(4, 12, games[0][0] + ' : ' + games[0][1] + ' ' * 5)
-    
+    scrn.addstr(7, 4, db['matches'][db['current_round']]['st'])
+    try:
+        scrn.addstr(4, 12, games[0][1] + ' : ' + games[0][0] + ' ' * 5)
+    except IndexError:
+        pass
+    try:
+        scrn.addstr(6, 12, games[1][1] + ' : ' + games[1][0] + ' ' * 5)
+    except IndexError:
+        pass
+    try:
+        scrn.addstr(7, 12, games[3][1] + ' : ' + games[2][0] + ' ' * 5)
+    except IndexError:
+        pass
+    try:
+        scrn.addstr(8, 12, games[3][1] + ' : ' + games[3][0] + ' ' * 5)
+    except IndexError:
+        pass
+    try:
+        scrn.addstr(5, 12, games[0][2] + ' ' * 5)
+        scrn.addstr(9, 4, str(db['current_round']) + ' ' + str(db['GM']))
+    except IndexError:
+        pass
+    if cmd_mode:
+        scrn.addstr(10, 4, str(cmd))
+    scrn.addstr(10, 4, out)
+    nxt = False
     # Refresh screens
     scrn.refresh()
     pygame.display.flip()
